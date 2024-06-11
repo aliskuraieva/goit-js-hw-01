2029f4a70deeb5a2c81c75dbd423ab8eef631a8d
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-06-11 13:37:16 +0300

    add script

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  const message = "You ordered ${quantity} droids worth ${totalPrice} credits!";
+  const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   return message;
 }
 
